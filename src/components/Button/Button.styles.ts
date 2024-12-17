fd97b4fd91b683c4e8374c31efcdbcbdb89c58ea
Brene <brendoleitepereira@gmail.com>
2024-12-17 15:08:38 -0300

    Fix: button adjustments

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -2,12 +2,23 @@ import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
   padding: 1rem;
-  background-color: ${({ theme }) => theme.colors.primary};
-  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.primary};
   border-radius: 0.3rem;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
 
-  width: 12rem;
+  width: auto;
   height: 4rem;
 
-  font-size: 1.2rem;
+  font-size: 0.8rem;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: white;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    transition:
+      background-color 0.2s ease-in-out,
+      color 0.2s ease-in-out;
+    cursor: pointer;
+  }
 `;
