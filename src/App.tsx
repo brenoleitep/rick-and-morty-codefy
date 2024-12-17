6749c2c777e549b28a6638554dbc44d5e2a6b8a7
Brene <brendoleitepereira@gmail.com>
2024-12-17 12:35:17 -0300

    Chore: routes setup full

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import "./App.css";
-import { useFetchCharacters } from "./hooks/useFetchCharacters";
+import MainRoutes from "./routes";
 
 function App() {
-  const { data, error, isLoading } = useFetchCharacters();
-
-  console.log(data);
-  if (error) return <p>Erro ao buscar personagens!</p>;
-
-  if (isLoading) return <p>Carregando...</p>;
-
   return (
-    <div>
-      <h1>Personagens de Rick and Morty</h1>
-
-      <ul>
-        {data?.results.map((character) => (
-          <li key={character.id}>
-            <h2>{character.name}</h2>
-            <p>{character.species}</p>
-            <img src={character.image} alt={character.name} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <MainRoutes />
+    </>
   );
 }
 
