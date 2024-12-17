ac2c7fa8db1867c160e80d39491466a4b3f01df5
Brene <brendoleitepereira@gmail.com>
2024-12-17 19:43:21 -0300

    Feat: create header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import logo from "../../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Rick and Morty logo" />
+
+      {/* <Navbar /> */}
+    </header>
+  );
+};
+
+export default Header;
