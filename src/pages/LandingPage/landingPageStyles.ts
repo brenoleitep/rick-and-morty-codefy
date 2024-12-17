23db193e2cc161f52095ba6d1c56759ddd926cdc
Brene <brendoleitepereira@gmail.com>
2024-12-17 15:49:01 -0300

    Fix: improve responsivity

--- src/pages/LandingPage/landingPageStyles.ts
+++ src/pages/LandingPage/landingPageStyles.ts
@@ -3,12 +3,20 @@ import styled from "styled-components";
 export const BackgroundContainer = styled.div`
   position: relative;
   width: 100%;
+  height: 100vh;
   padding: 2rem;
   display: flex;
   flex-direction: column;
   gap: 1rem;
   align-items: center;
   justify-content: center;
+  background-color: black;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    text-align: left;
+  }
 `;
 
 export const Content = styled.div`
@@ -17,18 +25,21 @@ export const Content = styled.div`
   gap: 1rem;
   position: relative;
   z-index: 1;
-  color: ${({ theme }) => theme.colors.text};
-  text-align: center;
+  color: ${({ theme }) => theme.colors.white};
 
   p {
     font-size: 0.6rem;
     line-height: 1.5;
     margin-bottom: 0.5rem;
   }
+
+  @media (min-width: 768px) {
+    width: 50%;
+  }
 `;
 
 export const SpanContainer = styled.span`
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
   font-family: "Press Start 2P", cursive;
   font-size: 1.2rem;
   text-shadow: 0px 0px 0px 1px;
@@ -38,10 +49,9 @@ export const SpanContainer = styled.span`
 export const TypingContainer = styled.h2`
   font-family: "Press Start 2P", cursive;
   font-size: 1rem;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
   text-shadow: 0px 0px 0px 1px;
   line-height: 1.5;
-  text-align: center;
 `;
 
 export const ImageContainer = styled.div<{
@@ -50,14 +60,26 @@ export const ImageContainer = styled.div<{
   width: 320px;
   height: auto;
   margin: 0 auto;
-  perspective: 800px; /* Define a profundidade para o efeito 3D */
+  perspective: 800px;
 
   img {
     width: 100%;
-    height: auto;
-    transition: transform 0.1s ease-out; /* Suaviza o movimento */
-    transform: ${({ movement }) =>
-      `rotateX(${(movement.y - window.innerHeight / 2) / 20}deg) 
-       rotateY(${(movement.x - window.innerWidth / 2) / 20}deg)`};
+    min-height: 360px;
+  }
+
+  @media (min-width: 768px) {
+    width: 50%;
+
+    img {
+      height: 460px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    width: 50%;
+
+    img {
+      height: 560px;
+    }
   }
 `;
