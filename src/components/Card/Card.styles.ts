2a23191a146d1a6bdf37b236b7a103812d3f32c1
Brene <brendoleitepereira@gmail.com>
2024-12-18 16:01:10 -0300

    Fix: adjust to receive button

--- src/components/Card/Card.styles.ts
+++ src/components/Card/Card.styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CardContainer = styled.div`
   width: 300px;
-  height: 400px;
+  height: 450px;
   background: linear-gradient(135deg, #0e2d50, #49774b, #0e2d50);
   display: flex;
   flex-direction: column;
